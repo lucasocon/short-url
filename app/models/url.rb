f222f03690e1528fc9a5a234a5ee5e9c07298849
Lucas Ocon <lucas_masterclas@hotmail.com>
2019-09-30 11:55:51 -0300

    Fix url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,31 +2,30 @@ class Url < ApplicationRecord
   #public_uid configuration
   generate_public_uid column: :code, generator: PublicUid::Generators::HexStringSecureRandom.new(4)
 
-  #validates
+  # Validations
   validates :long_url, presence: true, url: true
 
-  #callbacks
+  # Callbacks
   after_create :set_short_url
   after_create :set_title
 
-  #methods
   def increment_redirects_amount
     self.redirects_amount += 1
-    self.save!
+    save!
   end
 
   def self.top_100
-    Url.all.limit(100).order(redirects_amount: :desc)
+    all.limit(100).order(redirects_amount: :desc)
   end
 
   private
 
   def set_short_url
-    self.short_url = Rails.application.credentials.root_url + "/" + self.code
-    self.save!
+    self.short_url = "#{Rails.application.credentials.root_url}/#{code}"
+    save!
   end
 
   def set_title
-    ScrapingWorker.perform_async(self.id)
+    ScrapingWorker.perform_async(id)
   end
 end
